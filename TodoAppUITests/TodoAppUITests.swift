ae90cf41c6be414ffc87c78b85f5dc92da649e0a
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2024-07-28 13:28:41 +0530

    Writing Test to Save Task to Database

---
+++ TodoAppUITests/TodoAppUITests.swift
@@ -0,0 +1,41 @@
+//
+//  TodoAppUITests.swift
+//  TodoAppUITests
+//
+//  Created by Sajed Shaikh on 28/07/24.
+//
+
+import XCTest
+
+final class when_app_is_launched: XCTestCase {
+    
+    func test_should_not_display_any_task() {
+        let app = XCUIApplication()
+        continueAfterFailure = false
+        app.launch()
+        
+        let taskList = app.tables["taskList"]
+        XCTAssertEqual(0, taskList.cells.count)
+    }
+    
+}
+
+final class when_user_saves_a_new_task: XCTestCase {
+    
+    func test_should_be_able_to_display_task_successfully() {
+        let app = XCUIApplication()
+        continueAfterFailure = false
+        app.launch()
+        
+        let titleTextField = app.textFields["titleTextField"]
+        titleTextField.tap()
+        titleTextField.typeText("Mow the lawn")
+        
+        let saveTaskButton = app.buttons["saveTaskButton"]
+        saveTaskButton.tap()
+        
+        let taskList = app.tables["taskList"]
+        XCTAssertEqual(1, taskList.cells.count)
+    }
+    
+}
