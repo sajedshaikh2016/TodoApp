5ed900ebbbc766ac825b564f1ec114233f5d9599
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2024-07-28 12:54:08 +0530

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Sajed Shaikh on 28/07/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
